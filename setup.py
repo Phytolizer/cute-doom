cee0d5703eb34090ebfd97f25263d31aca5ec1bb
Kyle Coffey <me@phytolizer.dev>
2022-03-12 20:11:18 -0600

    allo

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="cute_doom",
+    version="0.1.0",
+    author="Kyle Coffey",
+    author_email="me@phytolizer.dev",
+    url="https://github.com/Phytolizer/cute-doom",
+    description="Cross-platform Doom sourceport",
+    license="MIT",
+    packages=["cute_doom"],
+)
